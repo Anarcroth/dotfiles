8df2c5a8e543302f4fab57809bc6bf79de0a5801
Martin Nestorov <mdinestorov@gmail.com>
2018-03-01 19:36:49 +0200

    Added command for seraching types of files

--- usefull_commands.sh
+++ usefull_commands.sh
@@ -2,3 +2,6 @@
 
 # export .md to .pdf
 pandoc --latex-engine=xelatex -V mainfont="Times New Roman" -V fontsize=12pt -V geometry:margin=1in -V documentclass=article -V linestrech=1.5 -V link-as-notes SOURCE.md -s -o DESTINATION.pdf
+
+# Search for suffix type files, but only print the paths to directories that contain them.
+find . -name '*.<suffix>' | sed 's/\/[^\/]*$//' | sort | uniq
