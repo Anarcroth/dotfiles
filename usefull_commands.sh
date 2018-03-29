3a89aa132b5f8914c26e7bf0b001f29f81262698
Martin Nestorov <mnestorov@protonmail.com>
2018-03-29 18:17:00 +0300

    Updated pdf conversion command line

--- usefull_commands.sh
+++ usefull_commands.sh
@@ -1,7 +1,7 @@
 # This is a list of useful commands that have aided me throughout my hacking
 
 # export .md to .pdf
-pandoc --latex-engine=xelatex -V mainfont="Times New Roman" -V fontsize=12pt -V geometry:margin=1in -V documentclass=article -V linestrech=1.5 -V link-as-notes SOURCE.md -s -o DESTINATION.pdf
+pandoc --latex-engine=xelatex --wrap=auto -p --highlight-style=kate -V mainfont="Times New Roman" -V fontsize=12pt -V geometry:margin=1in -V documentclass=article -V linestrech=1.5 -V colorlinks -V link-as-notes SRC.md -s -o DEST.pdf
 
 # Search for suffix type files, but only print the paths to directories that contain them.
 find . -name '*.<suffix>' | sed 's/\/[^\/]*$//' | sort | uniq
