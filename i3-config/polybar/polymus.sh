0ccd0ba40fd8b7e7e6d6d45ece5e75e805193350
Martin Nestorov <mdinestorov@gmail.com>
2018-04-07 20:28:09 +0300

    Added polybar configuration

---
+++ i3-config/polybar/polymus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+prepend_0 () {
+    seq -f "%02g" $1 $1
+}
+
+artist=$(echo -n $(cmus-remote -C status | grep artist -m 1 | cut -c 12-))
+song=$(echo -n $(cmus-remote -C status | grep title -m -1 | cut -c 11-))
+
+position=$(cmus-remote -C status | grep position | cut -c 10-)
+minutes1=$(prepend_0 $(( $position / 60 )))
+seconds1=$(prepend_0 $(( $position % 60 )))
+
+duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+minutes2=$(prepend_0 $(( $duration / 60 )))
+seconds2=$(prepend_0 $(( $duration % 60 )))
+
+status=$(echo -n $(cmus-remote -C status | grep status -m 1 | cut -c 8-))
+
+if [ "$status" == "playing" ]; then
+    echo -n "$artist - $song > $minutes1:$seconds1 - $minutes2:$seconds2"
+else
+    echo -n "$artist - $song | $minutes1:$seconds1 - $minutes2:$seconds2"
+fi
