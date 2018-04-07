4b3a5e9c725c836334cc8cf8748e2592fab9c834
Martin Nestorov <mnestorov@protonmail.com>
2018-04-07 20:36:39 +0300

    Added git command

--- usefull_commands.sh
+++ usefull_commands.sh
@@ -11,3 +11,6 @@ clear;while :;do echo $LINES $COLUMNS $(($RANDOM%$COLUMNS));sleep 0.1;done|gawk 
 
 # convert *.avi to *.mp4
 ffmpeg -y -i input_file.avi -s 432x320 -b:v 384k -vcodec libx264 -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+partp4x4+partp8x8 -subq 6 -trellis 0 -refs 5 -bf 0 -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -c:a aac -ac 1 -ar 16000 -r 13 -ab 32000 -aspect 3:2 -strict -2 output_file.mp4
+
+# Commit changes X days ago
+git commit --date="X day(s) ago" -m "MESSAGE"
