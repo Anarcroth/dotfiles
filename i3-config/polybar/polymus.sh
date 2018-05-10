65ee96a3a0579770618a7e4576527542ea176d35
Martin Nestorov <mdinestorov@gmail.com>
2018-05-10 19:53:56 +0300

    Formated polymus, added bar progression, WIP

--- i3-config/polybar/polymus.sh
+++ i3-config/polybar/polymus.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
 prepend_0 () {
-    seq -f "%02g" $1 $1
+  seq -f "%02g" $1 $1
 }
 
 artist=$(echo -n $(cmus-remote -C status | grep artist -m 1 | cut -c 12-))
 song=$(echo -n $(cmus-remote -C status | grep title -m -1 | cut -c 11-))
 
 position=$(cmus-remote -C status | grep position | cut -c 10-)
-if [ "$position" != "" ]; then
-    minutes1=$(prepend_0 $(( $position / 60 )))
-    seconds1=$(prepend_0 $(( $position % 60 )))
+if [[ "$position" != "" ]]; then
+  minutes1=$(prepend_0 $(( $position / 60 )))
+  seconds1=$(prepend_0 $(( $position % 60 )))
 
-    duration=$(cmus-remote -C status | grep duration | cut -c 10-)
-    minutes2=$(prepend_0 $(( $duration / 60 )))
-    seconds2=$(prepend_0 $(( $duration % 60 )))
+  duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+  minutes2=$(prepend_0 $(( $duration / 60 )))
+  seconds2=$(prepend_0 $(( $duration % 60 )))
 fi
 
 status=$(echo -n $(cmus-remote -C status | grep status -m 1 | cut -c 8-))
 
-if [ "$status" == "playing" ]; then
-    echo -n " $artist - $song > $minutes1:$seconds1 - $minutes2:$seconds2"
+bar=""
+
+i=1
+time1=$(( seconds1 + $(( minutes1 * 60 ))))
+time2=$(( seconds2 + $(( minutes2 * 60 ))))
+deltax=$(( $(( time1 - time2 )) / 20 ))
+
+if [[ "$status" == "playing" ]]; then
+#  if [[ time1 < deltax * i ]]; then
+#    bar+="-"
+#  fi
+#  echo -n "$bar"
+  echo -n " $artist - $song > $minutes1:$seconds1 - $minutes2:$seconds2"
 else
+  if [[ "$artist" == "" ]]; then
+    echo -n " "
+  else
     echo -n " $artist - $song | $minutes1:$seconds1 - $minutes2:$seconds2"
+  fi
 fi
