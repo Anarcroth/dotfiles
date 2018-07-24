329f52029ecbc0a8aad8652768c228bdb2467457
Martin Nestorov <mnestorov@protonmail.com>
2018-07-24 22:46:52 +0300

    Added multimonitor setup and support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Restart polybar from file quietly
-polybar -r -q -c $HOME/.config/polybar/penguin top &
-polybar -r -q -c $HOME/.config/polybar/penguin bottom
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -r -q -c $HOME/.config/polybar/penguin top &
+    MONITOR=$m polybar -r -q -c $HOME/.config/polybar/penguin bottom
+  done
+else
+  MONITOR=$m polybar -r -q -c $HOME/.config/polybar/penguin top &
+  MONITOR=$m polybar -r -q -c $HOME/.config/polybar/penguin bottom
+fi
