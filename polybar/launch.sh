219e396a734ffc2a94d67cd878863011a76a8793
Martin Nestorov <mnestorov@protonmail.com>
2019-06-13 14:35:27 +0300

    Adds bars for second monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,12 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -r -q -c $HOME/.config/polybar/one-dark top &
-    MONITOR=$m polybar -r -q -c $HOME/.config/polybar/one-dark bottom
-  done
-else
-  MONITOR=$m polybar -r -q -c $HOME/.config/polybar/one-dark top &
-  MONITOR=$m polybar -r -q -c $HOME/.config/polybar/one-dark bottom
+if xrandr --query | grep "HDMI-0 connected";
+then
+  polybar -r -q -c $HOME/.config/polybar/one-dark topL &
+  polybar -r -q -c $HOME/.config/polybar/one-dark bottomL &
+fi
+
+if xrandr --query | grep "eDP-1-1 connected";
+then
+  polybar -r -q -c $HOME/.config/polybar/one-dark topR &
+  polybar -r -q -c $HOME/.config/polybar/one-dark bottomR &
 fi
