365f14f4a85142fe3f0422f20f9db7f120d9032a
Martin Nestorov <mnestorov@protonmail.com>
2020-04-04 23:20:38 +0300

    Add rofi powermenu

---
+++ rofi/scripts/menu_powermenu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rofi_command="rofi -theme themes/menu/powermenu.rasi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# Options
+shutdown="襤"
+reboot="ﰇ"
+lock=""
+suspend="鈴"
+logout=""
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "祥 $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        bash lock
+        ;;
+    $suspend)
+        amixer set Master mute
+        systemctl suspend
+        ;;
+    $logout)
+        exit i3
+        ;;
+esac
