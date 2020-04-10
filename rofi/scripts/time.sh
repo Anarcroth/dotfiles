77e25c83e5d745d8b1d8f543d3ea124db9e72756
Martin Nestorov <mnestorov@protonmail.com>
2020-04-10 16:20:27 +0300

    Add full calendar view

--- rofi/scripts/time.sh
+++ rofi/scripts/time.sh
@@ -6,9 +6,26 @@ rofi_command="rofi -theme themes/time.rasi"
 TIME_DATE="$(date +"%a %d, %R")"
 DAY="$(date +"%d" | tr -d '0')"
 
-calendar="$(cal -m | sed '/^[[:space:]]*$/d')"
+month_num="$(date +"%m" | tr -d '0')"
+month_num=$(( ($month_num - 1) * 8 + 1 ))
+
+calendar=""
+months=("jan" "feb" "mar" "apr" "may" "jun" "jul" "aug" "sep" "oct" "nov" "dec")
+for m in "${months[@]}"
+do
+    curr_month="$(cal -m $m | sed '/^[[:space:]]*$/d')"
+    month_lines="$(cal -m $m | sed '/^[[:space:]]*$/d' | wc -l)"
+    # In order for the view to be even through the year calendar, on month that have 8 lines of text,
+    # they should have only one additional new-line at the end.
+    if (( $month_lines > 7 ))
+    then
+	calendar="$calendar$curr_month\n"
+    else
+	calendar="$calendar$curr_month\n\n"
+    fi
+done
 
 options="$calendar"
 
 ## Main
-chosen="$(echo -e "$options" | $rofi_command -p "$TIME_DATE" -dmenu -selected-row 1)"
+chosen="$(echo -e "$options" | $rofi_command -p "$TIME_DATE" -dmenu -selected-row $month_num)"
