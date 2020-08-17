816fac32ccf46489061c680c378b60c9c5bf7be7
Martin Nestorov <mnestorov@protonmail.com>
2020-08-17 09:34:28 +0300

    Make polybar launch check for gerex connection instead of hardcoded monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,17 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if xrandr --query | grep "HDMI-0 connected";
+if xrandr --query | grep "^HDMI.* connected";
 then
   polybar -r -q -c $HOME/dotfiles/polybar/one-dark.conf bottomL &
 fi
 
-if xrandr --query | grep "eDP-1-1 connected";
+if xrandr --query | grep "^eDP.* connected";
 then
   polybar -r -q -c $HOME/dotfiles/polybar/one-dark.conf bottomR &
 fi
 
-if xrandr --query | grep "DP-1-1 connected";
+if xrandr --query | grep "^DP.* connected";
 then
   polybar -r -q -c $HOME/dotfiles/polybar/one-dark.conf bottomM &
 fi
