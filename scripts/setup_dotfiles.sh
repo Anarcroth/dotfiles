6fce8ac38218a26b23f1f5dffd9835ee8fc275d8
Martin Nestorov <mnestorov@protonmail.com>
2020-08-20 17:47:39 +0300

    Use grep to match only first occ

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -3,35 +3,35 @@
 # Setup status-bar (polybar)
 echo "Updating polybar"
 # Update monitor variables
-DP_MONITOR=$(xrandr --query | grep "^DP.* connected" | awk '{ print $1 }')
-EDP_MONITOR=$(xrandr --query | grep "^eDP.* connected" | awk '{ print $1 }')
-HDMI_MONITOR=$(xrandr --query | grep "^HDMI.* connected" | awk '{ print $1 }')
+DP_MONITOR=$(xrandr --query | grep -m 1 "^DP.* connected" | awk '{ print $1 }')
+EDP_MONITOR=$(xrandr --query | grep -m 1 "^eDP.* connected" | awk '{ print $1 }')
+HDMI_MONITOR=$(xrandr --query | grep -m 1 "^HDMI.* connected" | awk '{ print $1 }')
 
-if [ $DP_MONITOR ]; then
+if [ "$DP_MONITOR" ]; then
     echo "Updating DP [$DP_MONITOR] monitor (middle)"
     sed -i 's/\(middle = \).*/\1'"$DP_MONITOR"'/' ~/dotfiles/polybar/one-dark.conf
 fi
 
-if [ $EDP_MONITOR ]; then
+if [ "$EDP_MONITOR" ]; then
     echo "Updating eDP [$EDP_MONITOR] monitor (right)"
     sed -i 's/\(right = \).*/\1'"$EDP_MONITOR"'/' ~/dotfiles/polybar/one-dark.conf
 fi
 
-if [ $HDMI_MONITOR ]; then
+if [ "$HDMI_MONITOR" ]; then
     echo "Updating HDMI [$HDMI_MONITOR] monitor (left)"
     sed -i 's/\(left = \).*/\1'"$HDMI_MONITOR"'/' ~/dotfiles/polybar/one-dark.conf
 fi
 
 # Update ethernet/wifi interfaces
-ETH_INTERFACE=$(nmcli device status | grep "ethernet " | awk '{ print $1 }')
-WIFI_INTERFACE=$(nmcli device status | grep "wifi " | awk '{ print $1 }')
+ETH_INTERFACE=$(nmcli device status | grep -m 1 "ethernet " | awk '{ print $1 }')
+WIFI_INTERFACE=$(nmcli device status | grep -m 1 "wifi " | awk '{ print $1 }')
 
-if [ $ETH_INTERFACE ]; then
+if [ "$ETH_INTERFACE" ]; then
     echo "Updating ethernet interface [$ETH_INTERFACE]"
     sed -i 's/ETH_HOLDER/'"$ETH_INTERFACE"'/' ~/dotfiles/polybar/one-dark.conf
 fi
 
-if [ $WIFI_INTERFACE ]; then
+if [ "$WIFI_INTERFACE" ]; then
     echo "Updating wifi interface [$WIFI_INTERFACE]"
     sed -i 's/WIFI_HOLDER/'"$WIFI_INTERFACE"'/' ~/dotfiles/polybar/one-dark.conf
 fi
