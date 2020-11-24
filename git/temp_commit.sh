10e792bb939a246f389f62c7b28bde2376f421c9
Martin Nestorov <mnestorov@protonmail.com>
2020-11-24 23:10:26 +0200

    Add git temp-commit script formatting

--- git/temp_commit.sh
+++ git/temp_commit.sh
@@ -19,7 +19,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [[ -z $tmp_branch ]]; then
-    echo -e "\nYou have not specified a temporary branch!\nAborting!"; exit 1;
+    echo -e "\nYou have not specified a temporary branch!\nAborting!"; exit 1
 fi
 
 # Prepend temp/ before the name branch, just to follow nomenclature
@@ -28,43 +28,47 @@ if [[ $tmp_branch != temp/* ]]; then
 fi
 # If the specified branch doesn't exist create it, otherwise add the changes to the temporary branch
 if [[ -z $(git branch --list $tmp_branch) ]]; then
-    echo "Creating temporary branch '$tmp_branch'"
+    echo -e "\nCreating temporary branch '$tmp_branch'"
     git branch $tmp_branch
 else
-    echo "'$tmp_branch' already exists!"
+    echo -e "\n'$tmp_branch' already exists!"
 fi
 
 # Commit code changes
-echo "Committing changes for temporary storage"
+echo -e "\nCommitting changes for temporary storage"
 git add .
 curr_branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ -z $message ]]; then
-    echo "Creating a default commit message"
     message="Temporary commit #$(git rev-list $curr_branch..$tmp_branch --count)"
+    echo -e "\nCreating default commit message: [$message]"
 fi
+
 # This breaks if you are committing in the middle of a branch. Should always execute on the top of a branch
+echo -e "\nCommitting..."
 git commit --no-verify -m "$message"
+echo
+
 # Go to the temp branch and directly apply the commit we just did
 git checkout $tmp_branch
+echo -e "\nApplying temporary commit..."
 git cherry-pick --allow-empty --keep-redundant-commits --strategy-option=theirs $curr_branch
 # Check if there is a difference between the two branches. We DON'T want any difference!
 if [[ ! -z $(git diff $tmp_branch $curr_branch &>/dev/null) ]]; then
-    echo "!!Warning!!"
-    echo "There are still some differences between your changes and the applied ones to the temporary branch!"
-    echo "Please inspect them before continuing further!!"
-    exit 1
+    echo -e "\n!!Warning!!\nThere are still some differences between your changes and the applied ones to the temporary branch!\nPlease inspect them before continuing further!!\n"; exit 1
 fi
 
 if [[ $to_push ]]; then
-    echo "Pushing temporary changes to remote"
+    echo -e "\nPushing temporary changes to remote\n"
     # This makes the assumption that origin is the remote. It COULD be something else...
     git push origin $tmp_branch
 fi
 
 # Go back to the original place HEAD was
+echo
 git switch -
 
 if [[ $clean_slate ]]; then
+    echo -e "\nResetting original branch to clean state. View '$tmp_branch' for the changes"
     git reset --hard HEAD^
 else
     # Reset the commit we just did
