0ac5d8a7c888ac82f64a450ea120e31c3f7cd254
Martin Nestorov <mnestorov@protonmail.com>
2020-11-24 23:03:19 +0200

    Add git temp-commit script validation
    Add help listing formatting and fix bug where branch diff was being evaluated as a number

--- git/temp_commit.sh
+++ git/temp_commit.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 function help {
-    echo -e "\nThis script will save your local changes to a temporary branch. Useful in cases where you are working on many changes and you don't know which will stick and which will go, but you still need to save your work and push it to remote before committing to anything specific."
+    echo -e "\nThis script will save your local changes to a temporary branch.\nUseful in cases where you are working on many changes and you don't know which will stick and which will go,\nbut you still need to save your work and push it to remote before committing to anything specific.\n"
     echo "usage: git temp-commit [-m|--message <commit message>] [-b|--branch <temp branch name>] [-p|--push]"
 }
 
-# TODO add option clean temp branch
 # Handle script arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -13,11 +12,15 @@ while [[ "$#" -gt 0 ]]; do
         -b|--branch) tmp_branch="$2"; shift ;;
         -p|--push) to_push=1; shift ;;
         -c|--clean-slate) clean_slate=1; shift ;;
-        -h|--help) help; exit 1 ;;
+        -h) help; exit 1 ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
+
+if [[ -z $tmp_branch ]]; then
+    echo -e "\nYou have not specified a temporary branch!\nAborting!"; exit 1;
+fi
 
 # Prepend temp/ before the name branch, just to follow nomenclature
 if [[ $tmp_branch != temp/* ]]; then
@@ -36,6 +39,7 @@ echo "Committing changes for temporary storage"
 git add .
 curr_branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ -z $message ]]; then
+    echo "Creating a default commit message"
     message="Temporary commit #$(git rev-list $curr_branch..$tmp_branch --count)"
 fi
 # This breaks if you are committing in the middle of a branch. Should always execute on the top of a branch
@@ -44,7 +48,7 @@ git commit --no-verify -m "$message"
 git checkout $tmp_branch
 git cherry-pick --allow-empty --keep-redundant-commits --strategy-option=theirs $curr_branch
 # Check if there is a difference between the two branches. We DON'T want any difference!
-if [[ ! -z $((git diff $tmp_branch $curr_branch &>/dev/null)) ]]; then
+if [[ ! -z $(git diff $tmp_branch $curr_branch &>/dev/null) ]]; then
     echo "!!Warning!!"
     echo "There are still some differences between your changes and the applied ones to the temporary branch!"
     echo "Please inspect them before continuing further!!"
